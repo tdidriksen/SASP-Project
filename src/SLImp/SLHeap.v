4ae64d32e21e98176e90cd45b1840aeb24463ba9
sualitu <sual@itu.dk>
2013-04-09 14:46:17 +0200

    Added more theorems about alloc, dealloc and read.

--- src/SLImp/SLHeap.v
+++ src/SLImp/SLHeap.v
@@ -23,6 +23,7 @@ Definition write (key value : nat) (heap : Heap) : Heap :=
     | None => heap
   end.
 
+(* Allocation *)
 Theorem allocation_cardinality : forall heap key,
   ~ In key heap -> S (cardinal heap) = cardinal (alloc key heap).
 Proof.
@@ -42,9 +43,27 @@ Proof.
   unfold alloc.
   intuition.
 Qed.
+
+(* Deallocation *)
+
+Theorem deallocation_cardinality : forall heap key,
+  In key heap -> cardinal heap = S (cardinal (dealloc key heap)).
+
+Theorem deallocate_key : forall heap key,
+  (dealloc key heap) [ key ] = None.
   
-  
-  
+Theorem deallocate_empty_heap : forall heap key,
+	Empty heap -> Empty (dealloc key heap). 
+
+(* Write *)
+
+(** Theorem write_sound : forall heap heap' key value,
+  In key heap -> write key value heap = heap'. *)
+
+(* Read *)
+
+Theorem read_sound : forall heap key value,
+  read key (write key value heap) = Some (Holds value).
   
 
 End Heap.
